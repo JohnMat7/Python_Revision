a7a1bdd2b353576a7a9d2795f47a78e50c35ac77
johnmat7 <joffyjoffy8@gmail.com>
2025-06-14 23:26:44 +0530

    added main.py task.py and .gitignore file

---
+++ main.py
@@ -0,0 +1,13 @@
+#Main.py 
+
+def main():
+    while True:
+        print("Hi John What you want to do with your shedule , These are your options : ")
+        print("1. Add a Task :")
+        print("2. Remove a Task :")
+        print("3. Show all Tasks :")
+        print("4. Exit :")
+
+
+if __name__ == "__main__":
+    main()
